1782005f63bccd0513c7a0474b777ac671623c69
Moreno, Daniel <deumolo@gmail.com>
2024-12-07 20:09:56 -0600

    Se creo el archivo config db, se instaló uui + bcrypt, se creo el archivo seed

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,33 @@
-import { db } from 'astro:db';
+import { Role, User, db } from 'astro:db';
+
+import { v4 } from 'uuid'
+import bcrypt from 'bcryptjs'
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	const roles = [
+		{ id: 'admin', name: 'Admin', createdAt: new Date() },
+		{ id: 'user', name: 'User', createdAt: new Date() },
+	];
+
+	const johnDoe = {
+		id: v4(),
+		name: 'John Doe',
+		email: 'john@gmail.com',
+		password: bcrypt.hashSync('password'),
+		createdAt: new Date(),
+		role: 'admin',
+	}
+	const janeDoe = {
+		id: v4(),
+		name: 'Jane Doe',
+		email: 'jane@gmail.com',
+		password: bcrypt.hashSync('password'),
+		createdAt: new Date(),
+		role: 'user',
+	}
+
+	await db.insert(Role).values(roles);
+	await db.insert(User).values([johnDoe, janeDoe]);
+
 }
