ab47a9fa722ae8a2539c2dcc83f26695fb25d969
Moreno, Daniel <deumolo@gmail.com>
2024-12-10 08:59:40 -0600

    Se creó un componente para mostrar todos los productos

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,9 +1,35 @@
 import type { ProductWithImages } from '@/interfaces/product-with-images.interface';
+import { useState } from 'react';
 
 interface Props {
   product: ProductWithImages;
 }
 
-export const ProductCard = ( { product } : Props) => {
-    return <div>{product.title}</div>
+export const ProductCard = ({ product }: Props) => {
+  const images = product.images.split(',').map((image) => {
+    return image.startsWith('http')
+      ? image
+      : `${import.meta.env.PUBLIC_URL}/images/products/${image}`;
+  });
+
+  const [currentImage, setCurrentImage] = useState(images[0]);
+
+  return (
+    <a
+      onMouseEnter={() => {
+        setCurrentImage(images[1]);
+      }}
+      onMouseLeave={() => {
+        setCurrentImage(images[0]);
+      }}
+    >
+      <img
+        src={currentImage}
+        alt={product.title}
+        className='h-[350px] object-contain'
+      />
+      <h4>{product.title}</h4>
+      <p>${product.price}</p>
+    </a>
+  );
 };
