83399f90f722d57bc4c00b577781e3f62c1b33a8
Moreno, Daniel <deumolo@gmail.com>
2024-12-11 17:40:30 -0600

    Se hizo cambios minimos a la navbar y mainlayout

--- src/components/cart/CartCounter.tsx
+++ src/components/cart/CartCounter.tsx
@@ -1,6 +1,6 @@
 export const CartCounter = () => {
   return (
-    <a href='' className="relative inline-block">
+    <a href='/cart' className="relative inline-block">
       <span className='absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5'>
         3
       </span>
