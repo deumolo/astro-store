21d6b5eee64fa14f71e27cdc35bc7428957fbd6e
Moreno, Daniel <deumolo@gmail.com>
2024-12-11 15:42:53 -0600

    Se crearon las funciones para manejar los datos en el carrito de compra

--- src/utils/cart-cookies.ts
+++ src/utils/cart-cookies.ts
@@ -1,11 +1,32 @@
+import type { CartItem } from "@/interfaces/cart-item";
+import Cookies from "js-cookie";
+
 export class CartCookiesClient {
-    static getCart() {
-        return [];
+    static getCart(): CartItem[] {
+        const cart = JSON.parse(Cookies.get("cart") || "[]");
+        return cart;
     }
-    static addItem() {
-        return [];
+    static addItem(cartItem: CartItem) {
+        const cart = CartCookiesClient.getCart();
+        const itemInCart = cart.find(item => item.productId === cartItem.productId && item.size === cartItem.size);
+
+        if (itemInCart) {
+            itemInCart.quantity += cartItem.quantity;
+        } else {
+            cart.push(cartItem);
+        }
+
+        Cookies.set("cart", JSON.stringify(cart));
+        return cart;
     }
-    static removeItem() {
-        return [];
+    static removeItem(cartItem: CartItem) {
+        const cart = CartCookiesClient.getCart();
+        const itemIndex = cart.findIndex(item => item.productId === cartItem.productId && item.size === cartItem.size);
+
+        if (itemIndex !== -1) {
+            cart.splice(itemIndex, 1);
+        }
+
+        return cart;
     }
 }
