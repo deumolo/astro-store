0f341533dca9f17f3b6eaacc3916bae4d8b603c3
Moreno, Daniel <deumolo@gmail.com>
2024-12-11 20:53:53 -0600

    Se creo la base para la página carrito de compra y se creo la clase Formatter

---
+++ src/utils/Formatter.ts
@@ -0,0 +1,10 @@
+export class Formatter {
+    static currency(value: number, decimals = 2): string {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals
+        }).format(value);
+    }
+}
