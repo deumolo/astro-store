227229c9cd7111da6b48a9e851a8605d69c05b2b
Moreno, Daniel <deumolo@gmail.com>
2024-12-13 16:30:02 -0600

    Se creo el server action para editar crear producto

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,9 @@
 import { loginUser, logout, registerUser } from './auth';
 import { getProductsByPage } from './products/get-products-by-page.action';
 import { getProductBySlug } from './products/get-products-by-slug';
+import { createUpdateProduct } from './products/create-update-product.action';
 import { loadProductsFromCart } from './cart/load-products-from-cart.action';
+
 
 export const server = {
   loginUser,
@@ -9,5 +11,6 @@ export const server = {
   registerUser,
   getProductsByPage,
   getProductBySlug,
-  loadProductsFromCart
+  loadProductsFromCart,
+  createUpdateProduct,
 };
